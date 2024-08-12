1c509dc4a43eee41db5eaa7e72e142f19b507cc1
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-08-13 00:44:23 +0530

    floating shape

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,34 @@
 import "./App.css";
+import FloatingShape from "./components/FloatingShape";
 
 function App() {
   return (
-    <h1 className="text-3xl text-red-800 font-bold underline">Hello world!</h1>
+    <div
+      className="min-h-screen bg-gradient-to-br
+  from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden"
+    >
+      <FloatingShape
+        color="bg-green-500"
+        size="w-64 h-64"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-emerald-500"
+        size="w-48 h-48"
+        top="70%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-lime-500"
+        size="w-32 h-32"
+        top="40%"
+        left="-10%"
+        delay={2}
+      />
+    </div>
   );
 }
 
